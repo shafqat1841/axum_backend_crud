191b7d4865ae2e86f2f3ad4c2583f384c17625b8
shafqat1841 <shafqat1841@gmail.com>
2026-06-30 20:58:13 +0500

    fix: make person_db file for PersonModelExt trait

---
+++ src/database/person_db.rs
@@ -0,0 +1,29 @@
+use async_trait::async_trait;
+
+use crate::{db::DBClient, models::PersonModel};
+
+#[async_trait]
+pub trait PersonModelExt {
+    async fn get_persons(&self) -> Result<Option<Vec<PersonModel>>, sqlx::Error>;
+}
+
+#[async_trait]
+impl PersonModelExt for DBClient {
+    async fn get_persons(&self) -> Result<Option<Vec<PersonModel>>, sqlx::Error> {
+        let query = r#"
+        SELECT  
+                 id, 
+                username, 
+                age, 
+                created_at, 
+                updated_at 
+        FROM persons 
+        "#;
+
+        let user: Vec<PersonModel> = sqlx::query_as::<_, PersonModel>(query)
+            .fetch_all(&self.pool)
+            .await?;
+
+        Ok(Some(user))
+    }
+}
