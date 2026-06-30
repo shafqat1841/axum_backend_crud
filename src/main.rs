5b3a6e1aa0cd4c45567fd47664fbaf2b4f9462fc
shafqat1841 <shafqat1841@gmail.com>
2026-06-30 19:33:12 +0500

    fix: make AppState struct and db pool and statred the arc app_state in create_routes route builder function parameter

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,48 @@
+mod config;
+mod database;
+mod db;
+mod models;
 mod router;
 
+use std::sync::Arc;
+
+use sqlx::postgres::PgPoolOptions;
+
+use crate::{config::Config, db::DBClient};
+
+#[derive(Debug, Clone)]
+pub struct AppState {
+    pub env: Config,
+    pub db_client: DBClient,
+}
 
 #[tokio::main]
 async fn main() {
+    let config = Config::init();
+
+    let pool = match PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&config.database_url)
+        .await
+    {
+        Ok(pool) => {
+            println!("✅Connection to the database is successful!");
+            pool
+        }
+        Err(err) => {
+            println!("🔥 Failed to connect to the database: {:?}", err);
+            std::process::exit(1);
+        }
+    };
+
+    let db_client = DBClient::new(pool);
+    let app_state = AppState {
+        env: config.clone(),
+        db_client,
+    };
+
     // build our application with a single route
-    let app_api = router::create_routes();
+    let app_api = router::create_routes(Arc::new(app_state.clone()));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
