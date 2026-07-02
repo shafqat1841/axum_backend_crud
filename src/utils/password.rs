df7fef9ab137374ba8a27b13a618071e3e7a2bf1
shafqat1841 <shafqat1841@gmail.com>
2026-07-02 20:58:49 +0500

    feat: make utils for poassword and token

---
+++ src/utils/password.rs
@@ -0,0 +1,68 @@
+use argon2::{
+    Algorithm, Argon2, Params, Version,
+    password_hash::{
+        PasswordHash,
+        PasswordHasher,
+        PasswordVerifier,
+        SaltString,
+    },
+};
+
+use rand_core::OsRng;
+
+use crate::errors::ErrorMessage;
+
+const MAX_PASSWORD_LENGTH: usize = 64;
+
+pub fn hash(password: impl Into<String>) -> Result<String, ErrorMessage> {
+    let password = password.into();
+
+    if password.is_empty() {
+        return Err(ErrorMessage::EmptyPassword);
+    }
+
+    if password.len() > MAX_PASSWORD_LENGTH {
+        return Err(ErrorMessage::ExceededMaxPasswordLength(MAX_PASSWORD_LENGTH));
+    }
+
+    let salt = SaltString::generate(&mut OsRng);
+
+    // Customize Argon2 parameters
+    let argon2 = Argon2::new(
+        Algorithm::Argon2id, // Use Argon2id for a balance of security and performance
+        Version::V0x13,      // Use the latest version
+        Params::new(
+            15 * 1024, // Memory cost (in KiB) - reduced from default (e.g., 32 * 1024)
+            2,         // Time cost (iterations) - reduced from default (e.g., 3)
+            1,         // Parallelism (threads) - reduced from default (e.g., 4)
+            None,      // Optional output length
+        )
+        .map_err(|_| ErrorMessage::HashingError)?,
+    );
+
+    let hashed_password = argon2
+        .hash_password(password.as_bytes(), &salt)
+        .map_err(|_| ErrorMessage::HashingError)?
+        .to_string();
+
+    Ok(hashed_password)
+}
+
+pub fn compare(password: &str, hashed_password: &str) -> Result<bool, ErrorMessage> {
+    if password.is_empty() {
+        return Err(ErrorMessage::EmptyPassword);
+    }
+
+    if password.len() > MAX_PASSWORD_LENGTH {
+        return Err(ErrorMessage::ExceededMaxPasswordLength(MAX_PASSWORD_LENGTH));
+    }
+
+    let parsed_hash =
+        PasswordHash::new(hashed_password).map_err(|_| ErrorMessage::InvalidHashFormat)?;
+
+    let password_matched = Argon2::default()
+        .verify_password(password.as_bytes(), &parsed_hash)
+        .map_or(false, |_| true);
+
+    Ok(password_matched)
+}
