1cb6972c0d192d689d5667624531b7ad9bf8dc37
shafqat1841 <shafqat1841@gmail.com>
2026-07-02 20:59:42 +0500

    fix: update persons route pathname and add auth routes

--- src/router/mod.rs
+++ src/router/mod.rs
@@ -1,18 +1,27 @@
+mod auth_routes;
 mod person_routes;
 
 use std::sync::Arc;
 
 use axum::{Extension, Router, routing::get};
 
-use crate::{AppState, router::person_routes::person_router};
+use crate::{
+    AppState,
+    router::{auth_routes::auth_router, person_routes::person_router},
+};
 
 pub fn create_routes(app_state: Arc<AppState>) -> axum::Router {
     let router = Router::new();
     let person_api = person_router();
-    let app_api = router
-        .route("/", get(home))
-        .nest("/api", person_api)
+    let auth_api = auth_router();
+    let api_route = router
+        .nest("/auth", auth_api)
+        .nest("/persons", person_api)
         .layer(Extension(app_state));
+
+    let home_route = Router::new().route("/", get(home));
+
+    let app_api = Router::new().merge(home_route).nest("/api", api_route);
     app_api
 }
 
